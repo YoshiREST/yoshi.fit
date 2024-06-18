e60349d1dbc05190ec0048bc8ce17e4c2b8ad566
David <38597755+hxruhime@users.noreply.github.com>
2024-06-17 23:23:13 -0700

    add productcard skeleton

--- src/app/container/product/ProductList.tsx
+++ src/app/container/product/ProductList.tsx
@@ -4,6 +4,8 @@ import React, { useEffect, useState } from "react";
 import ProductCard from "@component/ProductCard";
 import { TProductData, TProducts } from "printify.ts/lib";
 
+import {Skeleton} from "@nextui-org/skeleton";
+
 const getProducts = async (): Promise<TProductData[]> => {
     const response = await fetch('/api');
     return response.json();
@@ -11,6 +13,7 @@ const getProducts = async (): Promise<TProductData[]> => {
 
 const ProductList: React.FC = () => {
     const [products, setProducts] = useState<TProductData[]>([]);
+    const [isLoaded, setIsLoaded] = useState(false);
 
     useEffect(() => {
         getProducts()
@@ -20,13 +23,17 @@ const ProductList: React.FC = () => {
             .catch(error => {
                 console.error("Error fetching products:", error.message);
             });
+
+        setIsLoaded(true);
     }, []);
 
     return (
         <main className="p-4 mt-5">
             <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
                 {products.map((product: TProductData) => (
-                    <ProductCard product={product} key={product.id} {...product} />
+                    <Skeleton isLoaded={isLoaded}>
+                        <ProductCard product={product} key={product.id} {...product} />
+                    </Skeleton>
                 ))}
             </div>
         </main>
