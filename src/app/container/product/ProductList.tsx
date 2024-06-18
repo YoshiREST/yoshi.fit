b1f44dcb42ce7c41cf69c57d0bafdaaa46712824
David <38597755+hxruhime@users.noreply.github.com>
2024-06-17 23:24:25 -0700

    remove skeleton

--- src/app/container/product/ProductList.tsx
+++ src/app/container/product/ProductList.tsx
@@ -4,8 +4,6 @@ import React, { useEffect, useState } from "react";
 import ProductCard from "@component/ProductCard";
 import { TProductData, TProducts } from "printify.ts/lib";
 
-import {Skeleton} from "@nextui-org/skeleton";
-
 const getProducts = async (): Promise<TProductData[]> => {
     const response = await fetch('/api');
     return response.json();
@@ -31,9 +29,7 @@ const ProductList: React.FC = () => {
         <main className="p-4 mt-5">
             <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
                 {products.map((product: TProductData) => (
-                    <Skeleton isLoaded={isLoaded}>
                         <ProductCard product={product} key={product.id} {...product} />
-                    </Skeleton>
                 ))}
             </div>
         </main>
