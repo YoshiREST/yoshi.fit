83814db0a2f0b873fb84916395d7252c461e4707
httpsisbetter <119723641+httpsisbetter@users.noreply.github.com>
2024-06-20 19:45:42 -0700

    Consistent styling

--- src/app/tos/page.tsx
+++ src/app/tos/page.tsx
@@ -2,9 +2,9 @@
 
 const Page: React.FC = () => {
     return (
-        <div>
-            <h1>Terms of Service</h1>
-            <p>These are the terms of service</p>
+        <div className="p-6 bg-white text-gray-900">
+            <h1 className="text-3xl font-bold mb-4">Terms of Service</h1>
+            <p className="text-lg">These are the terms of service</p>
         </div>
     );
 }
