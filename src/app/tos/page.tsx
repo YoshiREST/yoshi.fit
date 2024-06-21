20e755dbdff578e15e510e2213b98118a9d5056c
httpsisbetter <119723641+httpsisbetter@users.noreply.github.com>
2024-06-20 20:17:48 -0700

    Update page.tsx

--- src/app/tos/page.tsx
+++ src/app/tos/page.tsx
@@ -2,8 +2,8 @@
 
 const Page: React.FC = () => {
     return (
-        <div className="p-6 bg-white text-gray-900">
-            <h1 className="text-3xl font-bold mb-4">Terms of Service</h1>
+        <div className="p-6 bg-white text-gray-900 min-h-screen">
+            <h1 className="text-4xl font-bold mb-6">Terms of Service</h1>
             <p className="text-lg">These are the terms of service</p>
         </div>
     );
